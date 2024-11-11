92e2f2552624e51490ace7a87cb27b089b2bc111
BorgesMath <66123393+BorgesMath@users.noreply.github.com>
2024-11-11 18:25:02 -0300

    Resolvi problema do tsc

---
+++ js/byteBank.js
@@ -0,0 +1,41 @@
+var saldo = 3000;
+var elementoSaldo = document.querySelector(".saldo-valor .valor");
+if (elementoSaldo != null) {
+    elementoSaldo.textContent = saldo.toString();
+}
+var elementoFormulario = document.querySelector(".block-nova-transacao form");
+elementoFormulario.addEventListener("submit", function (event) {
+    event.preventDefault();
+    if (!elementoFormulario.checkValidity()) {
+        alert("Por favor, preencha os campos ");
+        return;
+    }
+    var inputTipoTransacao = elementoFormulario.querySelector("#tipoTransacao");
+    var inputValor = elementoFormulario.querySelector("#valor");
+    var inputData = elementoFormulario.querySelector("#data");
+    var tipoTransicao = inputTipoTransacao.value;
+    var valor = inputValor.valueAsNumber;
+    var data = new Date(inputData.value);
+    if (isNaN(valor) || valor <= 0) {
+        alert("Por favor, insira um valor numérico válido!");
+        return;
+    }
+    if (tipoTransicao == "Depósito") {
+        saldo += valor;
+    }
+    else if (tipoTransicao == "Transferência" || tipoTransicao == "Pagamento de Boleto") {
+        saldo -= valor;
+    }
+    else {
+        alert("Tipo de Transação é inválido!");
+        return;
+    }
+    elementoSaldo.textContent = saldo.toString();
+    var novaTransacao = {
+        tipoTransicao: tipoTransicao,
+        valor: valor,
+        data: data
+    };
+    console.log(novaTransacao);
+    elementoFormulario.reset();
+});
